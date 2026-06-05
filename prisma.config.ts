8944df37c68cf58af0cc474de9ee2e82a4351ed8
nc.khesed-tek-systems <nc@khesed-tek-systems.org>
2026-06-05 10:46:02 -0500

    fix: move prisma generate to build step

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,7 @@
-export default {
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-      directUrl: process.env.DIRECT_URL,
-    },
+import { defineConfig } from "prisma/config";
+export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL,
+    directUrl: process.env.DIRECT_URL,
   },
-};
+});
